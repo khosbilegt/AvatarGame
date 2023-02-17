354693a2716a4f989f7d0278b778f374d96b47b2
khosbilegt <89562265+khosbilegt@users.noreply.github.com>
2023-02-17 20:30:45 +0800

    added grip checker

---
+++ Source/CombatGame/JsonReaderTemp.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JsonReaderTemp.h"
+
+// Sets default values
+AJsonReaderTemp::AJsonReaderTemp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AJsonReaderTemp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AJsonReaderTemp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
